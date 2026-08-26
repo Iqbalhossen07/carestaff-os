7b0a71902f73832f902329ca759c466b0359ac04
Md Iqbal Hossen <iqbal@Mds-Mac-mini.local>
2026-08-26 11:44:02 +0600

    chore: remove script

--- rewrite-manage-form.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'src/app/dashboard/emar/resident/[residentId]/manage/ManageMedicationsClient.tsx';
-let content = fs.readFileSync(file, 'utf8');
-
-// I will just replace the Instructions block to include the Status block next to it.
-// Currently Instructions is md:col-span-4 or md:col-span-2? Let's check the current instructions block.
